a61dafb019099214cbed0ba2a7f1e2fb2a17c7bd
Berk Alp Yakici <berkalp.y@gmail.com>
2019-06-28 14:28:59 -0500

    Add admin redirect

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin'))
 ]
