a5877d3913934af703dd4fc214ee5e9f4b90ce79
Berk Alp Yakici <berkalp.y@gmail.com>
2019-06-28 14:26:20 -0500

    Add notes model to store taken notes

--- cms/notes/models.py
+++ cms/notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Note(models.Model):
+    """
+    Model for individual notes.
+    """
+    title = models.CharField(max_length=100, null=True, blank=True)
+    author_account_id = models.IntegerField(null=True, blank=True)
+    google_doc_id = models.CharField(max_length=100, null=True, blank=True)
+    creation_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        """
+        Returns a human-readable string for the instance.
+        """
+        return self.title + " by " + str(self.author_account_id)
