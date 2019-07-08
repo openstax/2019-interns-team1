40fe742fc4272c47d12868a50d3767ce1338a1a5
Ashley DSouza <ash.1303.github@dsouzaville.com>
2019-07-08 10:43:30 -0500

    adds function parameters to generate_google_doc, adds skeleton for create_blank_doc

--- cms/notes/functions.py
+++ cms/notes/functions.py
@@ -1,8 +1,20 @@
-def generate_google_doc():
+def generate_google_doc(template, title):
     """
-    Generates a google document from template and returns
-    the document ID.
+    Generates a google document from given template and title
+    and returns the document ID.
     """
+    return None
+
+def create_blank_doc(title):
+    """
+    Generates a blank google document with the given title and
+    returns the document instance.
+    """
+    ## use 'create' method on documents collection, i.e.
+    # title = title
+    # body = {}
+    # doc = service.documents().create(body=body).execute()
+    # return doc
     return None
 
 def create_table(nrows, ncols):
@@ -11,4 +23,5 @@ def create_table(nrows, ncols):
     rows and cols
     """
     return None
+
 
