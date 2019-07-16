bb0e2b53aa767367c49ce6cdc557a106ebf9f1d9
Berk Alp Yakici <berkalp.y@gmail.com>
2019-07-16 10:58:24 -0500

    FE Quick Fix

--- webview/dashboard.js
+++ webview/dashboard.js
@@ -14,3 +14,26 @@ $('.textBox').each(function (i) {
         clickable = true;
     })
 })
+
+
+$.ajax({
+    url: "http://localhost:8000/api/notes/",
+    context: document.body
+}).done(function(response) {
+    response.forEach(function(element) {
+        console.log(element);
+        $("#recentfiles").append(`
+        <div class = 'col-md-3'>
+            <div class="card" style="width: 13rem; height: 14rem;">
+                <a href="${element.google_doc_url}" target='_blank'>
+                <div class="card-body">
+                    <img src="images/word.png" class="card-img-top" alt="...">
+                    <p class="card-text text-center">${element.title}</p>
+                </div>
+                </a>
+            </div>
+        </div>
+        `);
+    });
+    
+});
