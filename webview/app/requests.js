d9f4bcf295bace6cee76cd36132160b74a58f56f
Berk Alp Yakici <berkalp.y@gmail.com>
2019-08-01 09:46:16 -0500

    2 notes per row

--- webview/app/requests.js
+++ webview/app/requests.js
@@ -43,10 +43,10 @@ function loadNotes(title="", filtertags="") {
         $("#recent-notes").html('');
         response.forEach(function(element) {
             $("#recent-notes").append(`
-            <div class="col-lg-4 d-flex">
+            <div class="col-lg-6 d-flex">
                 <div class="card mb-3">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <a href="${element.google_doc_url}" target="_blank" onclick="updateLastOpened(note='${element.id}');">
                                 <docicon>
                                     <span class="fa-layers fa-fw">
@@ -56,7 +56,7 @@ function loadNotes(title="", filtertags="") {
                                 </docicon>
                             </a>
                         </div>
-                        <div class="col-md-8">
+                        <div class="col-md-9">
                             <div class="card-body">
                                 <a href="${element.google_doc_url}" target="_blank" onclick="updateLastOpened(note='${element.id}');">
                                     <h5 class="card-title mb-0">${element.title}</h5>
